c47d8d9a3f408f861c132ad3da6ee088174ed4ba
Song Liu <song.liu@bristol.ac.uk>
2019-08-31 03:02:46 +0100

    added SDRE function with demo.py code

--- script/DRE/demo.py
+++ script/DRE/demo.py
@@ -6,18 +6,31 @@ from autograd.numpy import *
 from autograd import elementwise_grad as grad
 from autograd import jacobian as jacobian
 from scipy.optimize import minimize, Bounds, NonlinearConstraint
+from matplotlib import pyplot as plt
 
-d = 5
-n = 500
+d = 1
+n = 1000
 
 # Dataset
 XData = random.standard_normal((d, n))
 
 # Unnormalized density
-logpBar = lambda x:-sum(x**2- ones([d,1])*.2*x, 0) / 2
+logpBar = lambda x:-sum(x**2- .5*ones([d,1])*x, 0) / 2
 # f in Stein feature
-f = lambda X: X
+f = lambda X: vstack([X,X**2,X**3])
+b = 3*d
 
 # Estimate density ratio parameters
-delta = estimation(logpBar, f, XData)
+delta = estimation(logpBar, f, XData, solver="")
+
+grad_logp = grad(logpBar)
 print(delta)
+
+# estimated ratio function
+r = lambda x: delta.T.dot(steinFea(x, gradient_F(f, x), grad_logp(x), f, b)) + 1
+xMarks = array([linspace(-2.5,3.5)])
+
+print(r(xMarks))
+plt.plot(xMarks[0,:],r(xMarks)[0,:])
+plt.show()
+
