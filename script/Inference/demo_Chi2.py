77e73752844d97b04db90a00538351ce6de13b33
lamfeeling <song.liu@bristol.ac.uk>
2019-10-20 13:15:14 +0100

    updated chi2 experiment

--- script/Inference/demo_Chi2.py
+++ script/Inference/demo_Chi2.py
@@ -1,29 +1,25 @@
 #%% 
 import sdre
 from sdre.helper import *
-from sdre.estimators import primal
 from sdre.estimators import dual
 from scipy import stats
 from scipy import io
 
 from autograd.numpy import *
-from autograd import elementwise_grad as grad
-from autograd import jacobian as jacobian
-from scipy.optimize import minimize, Bounds, NonlinearConstraint
 from matplotlib import pyplot as plt
 
 from multiprocessing import Pool
 
-d = 20
-n = 500
-dTheta = d-1
+d = 5
+n = 250
+dTheta = d-4
 
 #%%
 # Unnormalized density parameterized by theta
 logpBar = lambda x,theta:-sum(x**2, 0) / 2 + \
-            sum(reshape(theta,[dTheta,1])*ones([dTheta,1])*x[:dTheta,:],0)
+            sum(reshape(theta,[dTheta,1])*ones([dTheta,1])*tanh(x[:dTheta,:]),0)
 # f in Stein feature
-f = lambda X: vstack([X])
+f = lambda X: vstack([tanh(X)])
 b = d
 
 #%% Show asymptotic distribution of Log-likelihood
@@ -38,13 +34,15 @@ def infer(seed):
     return LL
 
 if __name__ == '__main__':
-    # infer(0)
-    # LL = list(map(infer, range(1000)))
+    # LL = infer(0)
+    # LL = 2*array(list(map(infer, range(20))))
 
+    # if you want to be parallel...
+    LL = 2*array(list(Pool(16).map(infer, range(1000))))
+    
+    # output results to matlab file
+    io.savemat('LL.mat',{'LL':LL}) 
 
-    LL = 2*array(list(Pool(12).map(infer, range(10000))))
-#%%
-# rvs = stats.chi2.rvs(2,size=100)
-# res = stats.probplot(LL,dist=stats.chi2, sparams=(2,), plot=plt)
-# n, bins, patches = plt.hist(LL, 50, density=True, facecolor='g')
-    io.savemat('LL.mat',{'LL':LL})
+    # plot results    
+    res = stats.probplot(LL,dist=stats.chi2, sparams=(d-dTheta,), plot=plt)
+    plt.show()
